48217994b213a80a5ff3005ea045aa2cbbc3756a
adriank0h <adriankohcl01@gmail.com>
2024-01-16 16:14:30 +0800

    added new stuff

--- 23_object_oriented_programming/code.py
+++ 23_object_oriented_programming/code.py
@@ -55,5 +55,39 @@
 # student = Student("Bob", 36, 67, 90, 100, 100)
 # print(student.average())
 
-class student: 
+class Student: 
+    def __init__(self,name,grades):
+        self.name = name
+        self.grades = grades
+        
+    def average(self):
+        return sum(self.grades)/len(self.grades)
+        
+student = Student("Bob",(100,90,93,78,90))
+student2 = Student("FRaser",(10,90,93,78,90))
+student.age = 12
+
+student.child = ''
+if student.age > 11:
+    student.child = True
+else:
+    student.child = False
+
+# if student.child == True:
+#     print(f'{student.name} is a child')
+# else:
+#     print(f'{student.name} is a not child')
+
+#this is calling the average method that is inside the class Student and the self will be student which contants .name/.grades
+# print(Student.average(student))
+
+#does the same lol
+print(student.name)
+print(student.grades)
+print(student.average())
+
+print(student2.name)
+print(student2.grades)
+print(student2.average())
     
+
